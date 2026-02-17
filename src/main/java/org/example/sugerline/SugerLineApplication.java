cabde13de198a438dfd8c900ca8c4e207a60dff2
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-02-17 14:54:45 +0000

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/org/example/sugerline/SugerLineApplication.java
@@ -0,0 +1,13 @@
+package org.example.sugerline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugerLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugerLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sugerline/SugerLineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sugerline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugerLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
