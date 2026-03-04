4473e1dd7499a2ea5951062aa5e2f644bab842d0
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-03-04 13:23:36 +0000

    add Commande and CommandeLine repositories; implement JpaRepository interfaces for data access

---
+++ src/main/java/org/example/sugerline/repository/CommandeLineRepository.java
@@ -0,0 +1,7 @@
+package org.example.sugerline.repository;
+
+import org.example.sugerline.entity.CommandeLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommandeLineRepository extends JpaRepository<CommandeLine , Long> {
+}
